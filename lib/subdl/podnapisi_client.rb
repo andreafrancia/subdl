0147a9742232f864dc3fee9081535124b8febf94
Andrea Francia <andrea@andreafrancia.it>
2013-03-24 22:46:05 +0100

    Parse subtitle details page on podnapisi

---
+++ lib/subdl/podnapisi_client.rb
@@ -0,0 +1,37 @@
+require 'mechanize'
+
+class PodnapisiClient
+  def search text
+    agent = Mechanize.new
+    agent.get 'http://www.sub-titles.net/' do |page|
+        form = page.form_with( :name => 'sf3') do |search|
+          search.sK = text
+        end
+        return form.submit.body
+    end
+  end
+
+  def parse html
+    @doc = Nokogiri::HTML(html)
+  end
+  def subtitles
+    return @doc.css('tr.a td').map do |cell|
+      SearchResultRow.new cell
+    end
+  end
+  def zip_location
+    box = @doc.css('div.box')
+    box.css('a @href').first.value
+  end
+
+  class SearchResultRow
+    def initialize cell
+      @cell = cell
+    end
+    def title()    @cell.css('a[2]').text                    end
+    def href()     @cell.css('a[2]').attribute('href').value end
+    def filename() @cell.css('span.opis[1]').text            end
+    def season()   @cell.css('span.opis[2] b[1]').text       end
+    def episode()  @cell.css('span.opis[2] b[2]').text       end
+  end
+end
---
+++ spec/unit/podnapisi_client_spec.rb
@@ -0,0 +1,33 @@
+require 'subdl/podnapisi_client'
+require 'pry'
+
+describe PodnapisiClient do
+  let(:client) { PodnapisiClient.new }
+  context 'subtitles file details page' do
+    it 'should parse location to zip' do
+      client.parse File.read('spec/fixtures/podnapisi-details-page.html')
+      expect(client.zip_location).to eq '/en/ppodnapisi/download/i/1655415/k/2a90f7d65306efde18117ddefab31391052c2660'
+    end
+  end
+  context 'search result page' do
+    before :each do
+      client.parse File.read('spec/fixtures/podnapisi-search-result.html')
+      @subtitle = client.subtitles.first
+    end
+    it 'should parse title' do 
+      expect(@subtitle.title).to eq 'The 4400 (2004)'
+    end
+    it 'should parse filename' do
+      expect(@subtitle.filename).to eq 'The.4400.S02E06.DSR.XviD-TCM.'
+    end
+    it 'should parse season' do
+      expect(@subtitle.season).to eq '2'
+    end
+    it 'should parse episode' do
+      expect(@subtitle.episode).to eq '6'
+    end
+    it 'should parse link' do
+      expect(@subtitle.href).to eq '/en/ppodnapisi/podnapis/i/2192291/the-4400-2004-undertexter'
+    end
+  end
+end
