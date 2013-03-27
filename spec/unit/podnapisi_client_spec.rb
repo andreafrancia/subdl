4447618a6f572a29533d412519b4e6a7d037d82a
Andrea Francia <andrea@andreafrancia.it>
2013-03-27 19:28:21 +0100

    Fix test name

--- spec/unit/podnapisi_client_spec.rb
+++ spec/unit/podnapisi_client_spec.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 describe PodnapisiClient do
   let(:client) { PodnapisiClient.new }
-  context 'subtitles file details page' do
+  context 'subtitles details page' do
     it 'should parse location to zip' do
       client.parse File.read('spec/fixtures/podnapisi-details-page.html')
       expect(client.zip_location).to eq '/en/ppodnapisi/download/i/1655415/k/2a90f7d65306efde18117ddefab31391052c2660'
