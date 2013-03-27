db90c8182460eb709cbb84e8a3fff4890644484b
Andrea Francia <andrea@andreafrancia.it>
2013-03-27 19:29:54 +0100

    Clean tests

--- spec/integration/itasa_spec.rb
+++ spec/integration/itasa_spec.rb
@@ -4,20 +4,15 @@ require 'subdl'
 describe Itasa do
   let(:itasa) { Itasa.new }
 
-  describe 'a new itasa' do
-    it 'should not have been logged yet' do
-      itasa.should_not be_logged_in
-    end
-    it 'should login' do
-      itasa = Itasa.new
-      credentials = Credentials.new.read
-      itasa.login *credentials
-      itasa.should be_logged_in
-    end
-    it 'should discover ids' do
-      found_ids = itasa.search 'The Simpsons 24x15' 
-      found_ids.should == ['40039', '40038', '40037']
-    end
+  it 'should login' do
+    itasa.should_not be_logged_in
+    credentials = Credentials.new.read
+    itasa.login *credentials
+    itasa.should be_logged_in
+  end
+  it 'should discover ids' do
+    found_ids = itasa.search 'The Simpsons 24x15'
+    found_ids.should == ['40039', '40038', '40037']
   end
 end
 
