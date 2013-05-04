b2d50c62b84c114691771b10e3f578c9f9deb7b2
Andrea Francia <andrea@andreafrancia.it>
2013-05-04 22:55:34 +0200

    Add test for warning when no itasa subtitles.

--- spec/subdl_spec.rb
+++ spec/subdl_spec.rb
@@ -1,44 +1,67 @@
 describe 'Subtile download' do
   subject { subdl }
-  let(:subdl) { Subdl.new agent, stdout, file_system }
+  let(:subdl) { Subdl.new itasa, stdout, file_system }
 
-  let(:agent)       { double 'agent' }
+  let(:itasa)       { double 'itasa' }
   let(:stdout)      { StringIO.new }
   let(:file_system) { double 'file_system' }
 
   specify 'download from italian-subs.net' do
 
-    given_GET_response(
+    expect_GET(
       'http://www.italiansubs.net/modules/mod_itasalivesearch/search.php?' +
       'term=Show+1x02',
       '[{"value":"Title","id":"111"},{"value":"Title","id":"222"}]')
-    agent.should_receive(:login).with('username','password')
-    agent.should_receive(:login).with('username','password')
-    given_GET_response(
+    file_system.should_receive(:read_expand).with(
+      "~/.itasa-credentials").and_return("username\npassword")
+    itasa.should_receive(:login).with('username','password')
+    itasa.should_receive(:login).with('username','password')
+    expect_GET(
       'http://www.italiansubs.net/index.php?option=com_remository&Itemid=6'+
       '&func=fileinfo&id=111',
       '<a href="zip_location1"><img src=".../download2.gif"></a>')
-    given_GET_response(
+    expect_GET(
       'http://www.italiansubs.net/index.php?option=com_remository&Itemid=6'+
       '&func=fileinfo&id=222',
       '<a href="zip_location2"><img src=".../download2.gif"></a>')
-    given_GET_response('zip_location1', a_zip)
-    given_GET_response('zip_location2', a_zip)
+    expect_GET('zip_location1', a_zip)
+    expect_GET('zip_location2', a_zip)
     file_system.should_receive(:save_file).with(
       "Show.S01E02.avi.itasa.srt", "contents")
     file_system.should_receive(:save_file).with(
       "Show.S01E02.avi.itasa.1.srt", "contents")
-    file_system.should_receive(:read_expand).with(
-      "~/.itasa-credentials").and_return("username\npassword")
 
     subdl.main ["Show.S01E02.avi"]
 
-    stdout.string.lines.map(&:chomp).should include(
-      "Downloaded as Show.S01E02.avi.itasa.srt")
+    output_lines.should include "Downloaded as Show.S01E02.avi.itasa.srt"
   end
 
-  def given_GET_response url, response_body
-    agent.should_receive(:get).with(url).and_return(a_page(response_body))
+  describe 'when ITASA does not have subtitles' do
+    before do
+      given_GET(
+        'http://www.italiansubs.net/modules/mod_itasalivesearch/search.php?' +
+        'term=Show+1x03',
+        'null')
+    end
+
+    it 'should warn the user' do
+
+      subdl.main ["Show.S01E03.avi"]
+
+      output_lines.should include(
+        'No subtitles found on ITASA for: Show.S01E03.avi')
+    end
+  end
+
+  def output_lines
+    stdout.string.lines.map &:chomp
+  end
+
+  def expect_GET url, response_body
+    itasa.should_receive(:get).with(url).and_return(a_page(response_body))
+  end
+  def given_GET url, response_body
+    itasa.stub(:get).with(url).and_return(a_page(response_body))
   end
 
   def a_page content
